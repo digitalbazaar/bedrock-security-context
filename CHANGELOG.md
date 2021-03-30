14682665f207fe7795f5a297b703739b37cdc123
Matthew Collier <mcollier@digitalbazaar.com>
2021-03-30 14:29:06 -0400

    Support new key contexts.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # bedrock-security-context ChangeLog
 
+## 4.0.0 - TBD
+
+### Changed
+- **BREAKING**: Context URLs are no longer added to `bedrock.config.constants`.
+
+### Added
+- Support for `ed25519-signature-2018-context`.
+- Support for `ed25519-signature-2020-context`.
+- Support for `x25519-key-agreement-2020-context`.
+
 ## 3.0.0 - 2019-11-13
 
 ### Changed
--- test/mocha/01-api.js
+++ test/mocha/01-api.js
@@ -3,23 +3,29 @@
  */
 'use strict';
 
-const {config: {constants: bedrockConstants}} = require('bedrock');
-const {contexts, constants: contextConstants} = require('security-context');
 const {documentLoader} = require('bedrock-jsonld-document-loader');
 
 describe('bedrock-security-context', () => {
   it('sets up contexts properly', async () => {
-    for(const c in contextConstants) {
-      // ensure that constant vars are defined
-      should.exist(bedrockConstants[c]);
-      bedrockConstants[c].should.be.a('string');
-      bedrockConstants[c].should.equal(contextConstants[c]);
-      // ensure that context documents are defined
-      const result = await documentLoader(contextConstants[c]);
-      should.exist(result);
-      should.exist(result.document);
-      result.document.should.be.an('object');
-      result.document.should.eql(contexts.get(contextConstants[c]));
+    const testContexts = [
+      'security-context',
+      'ed25519-signature-2018-context',
+      'ed25519-signature-2020-context',
+      'x25519-key-agreement-2020-context',
+    ];
+    for(const testContext of testContexts) {
+      const {contexts, constants: contextConstants} = require(testContext);
+      for(const c in contextConstants) {
+        if(!c.includes('URL')) {
+          continue;
+        }
+        // ensure that context documents are defined
+        const result = await documentLoader(contextConstants[c]);
+        should.exist(result);
+        should.exist(result.document);
+        result.document.should.be.an('object');
+        result.document.should.eql(contexts.get(contextConstants[c]));
+      }
     }
   });
 });
